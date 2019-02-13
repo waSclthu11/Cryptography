b0e234964fc52b0113c1219497dd2d39d1e8debf
waSclthu11 <46973645+waSclthu11@users.noreply.github.com>
2019-02-13 12:53:21 -0500

    Updated from Brython Server: 2/13/2019 12:53:20 PM

--- cryptography.py
+++ cryptography.py
@@ -10,3 +10,4 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+strlist=([associations[w:w+1] for w in range(0, 26, 1)]) # Converts to list
