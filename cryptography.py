6390888de44a9789874592a62c34f4225805f971
waSclthu11 <46973645+waSclthu11@users.noreply.github.com>
2019-02-14 19:12:02 -0500

    Updated from Brython Server: 2/14/2019 7:12:01 PM

--- cryptography.py
+++ cryptography.py
@@ -11,3 +11,12 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 strlist=([associations[w:w+1] for w in range(0, 26, 1)]) # Converts to list
+text="test"
+key="hi"
+textlist=[]
+x=0 
+while x<len(text):
+    y=associations.find(text[x])
+    textlist.append(y)
+    x=x+1
+print (textlist)
